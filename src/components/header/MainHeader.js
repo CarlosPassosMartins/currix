d9d56bef4ed00c8505758b3f215eac6c8f4e7e66
carlospmart <carlospamartins@gmail.com>
2026-07-02 17:14:08 -0300

    ajustando os href para que eles tenham uma rota

--- src/components/header/MainHeader.js
+++ src/components/header/MainHeader.js
@@ -4,14 +4,14 @@ import logo from '../../imgs/icons/Logo.svg';
 function MainHeader(){
         return(
             <header className="header-main">
-                <a href='#' className="header-logo">
+                <a href='/' className="header-logo">
                         <img className='header-img' src={logo} alt="logo-currix"/>
                         <span className='header-title'><span className='header-title_C'>C</span>urrix</span>
                 </a>
 
                 <nav className="header-nav">
-                    <a className="header-nav_item" href="#">Templates</a>
-                    <a className="header-nav_item" href="#">Editor</a>
+                    <a className="header-nav_item" href="/templates">Templates</a>
+                    <a className="header-nav_item" href="/editor">Editor</a>
                 </nav>
             </header>
         );
